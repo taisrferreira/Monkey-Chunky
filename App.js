c991fc81e924ed9e87922dcfc249e438f86f8480
Taís Ribeiro Ferreira <94585719+taisrferreira@users.noreply.github.com>
2022-10-06 22:25:31 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      displayText: '',
+    };
+  }
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Header
+            backgroundColor={'#9c8210'}
+            centerComponent={{
+              text: 'Macaquinho fofo',
+              style: { color: '#fff', fontSize: 20 },
+            }}
+          />
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={(text) => {
+              this.setState({ text: text });
+            }}
+            value={this.state.text}
+          />
+          <TouchableOpacity
+            style={styles.goButton}
+            onPress={() => {
+              this.setState({ displayText: this.state.text });
+            }}>
+            <Text style={styles.buttonText}>IR</Text>
+          </TouchableOpacity>
+          <Text>{this.state.displayText}</Text>
+        </View>
+      </SafeAreaProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 200,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+});
+
